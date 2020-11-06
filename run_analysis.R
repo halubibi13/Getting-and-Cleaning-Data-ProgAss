8080666e3e278e9cff71560c4f9d9d738d91c7bd
Patrick Nuguid <phnuguid@up.edu.ph>
2020-11-06 18:56:08 +0800

    give col names, filter cols

--- run_analysis.R
+++ run_analysis.R
@@ -16,3 +16,16 @@ sum(colSums(is.na(y_train)))
 # merge train and test datasets
 df_vars <- rbind(x_train, x_test)
 df_label <- rbind(y_train, y_test)
+df_final <- cbind(df_vars, df_label)
+
+# get feature names
+features <- read.table("features.txt")
+# add additional column label for target variable
+new_features <- rbind(features, list(562L, "targetActivity"))
+
+# give the final dataframe column labels
+names(df_final) <- new_features[[2]]
+
+# extract mean and std columns
+columns_to_retain <- grep("mean|std", names(df_final))
+df_mean_std <- df_final[columns_to_retain]
